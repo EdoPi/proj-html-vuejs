f23e6ae7be37f86da6375ccb1170272f20f6e3c8
Edoardo Pizzicannella <edoardo.pizzicannella@gmail.com>
2021-02-19 12:51:16 +0100

    Milestone 2 - partial

--- js/script.js
+++ js/script.js
@@ -1,10 +1,30 @@
 var app = new Vue({
     el: '#root',
     data: {
-      message:'ciao',
+      myMenu:[
+          'Home',
+          'Apple',
+          'Microsoft',
+          'Android',
+          'Forums',
+          'Contact us',
+          'Join Us',
+      ],
+      active: false,
+      
     },
     methods:{
-      
+        isLast: function (index, array) {
+            if (index + 1 === array.length ) {                
+                return 'btn-black' 
+            }  
+        },
+        isToUpper: function (index, element, array ){
+            if (index + 1 === array.length) {
+                return element.toUpperCase();
+            }
+            return element;
+        }        
     }
   });
   
