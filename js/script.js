27265046a3e2520e719ebb97ee54a03dc3d36e3f
Edoardo Pizzicannella <edoardo.pizzicannella@gmail.com>
2021-02-22 18:00:37 +0100

    Milestone 4 - end

--- js/script.js
+++ js/script.js
@@ -1,6 +1,7 @@
 var app = new Vue({
     el: '#root',
     data: {
+
       myMenu:[
           'Home',
           'Apple',
@@ -11,6 +12,48 @@ var app = new Vue({
           'Join Us',
       ],
       active: false,
+      myFooter:[
+          {
+            title:'AVADA TECH FORUM',
+            description:'Sed semper, nisl id feugiat eleifend, erat est digssim felis, qui auctor elit enim nec ante. Cras et nibh risus. Pellentesque non mi sit amet mi intermentum.',
+            item:[],
+            url:'#1',
+          },
+          {
+            title:'POPULAR TOPICS',
+            description:'',
+            item:[
+                'Nam a dolor volutpat massa auctor semper',
+                'Sed euismond nunc urna fermentum arcu dapibus fringilla',
+                'Cras lacinia tellus id mauris finibus lacus molestie',
+                'Curabitur a scelerisqye ligula',
+            ],
+            url:'#2',
+          },
+          {
+            title:'RECENT TOPICS',
+            description:'',
+            item:[
+                'Cras lacinia tellus id mauris finibus lacus molestie',
+                'Proin at ligula sagittis vestibulum nisi vitae',
+                'Sed nec metus at est tincidunt elementum',
+                'Nulla egestas nulla eu nulla scipit molestie',
+
+            ],
+            url:'#3',
+          },
+          {
+            title:'LATEST REPLIES',
+            description:'',
+            item:[
+                'Cras lacinia tellus id mauris finibus lacus molestie',
+                'Cras lacinia tellus id mauris finibus lacus molestie',
+                'Nulla egestas nulla eu nulla scipit molestie',
+                'Mauris ac nibh quis eros sagittis lacinia a et dui',
+            ],
+            url:'#3',
+          },
+      ],
       
     },
     methods:{
@@ -24,8 +67,11 @@ var app = new Vue({
                 return element.toUpperCase();
             }
             return element;
-        }        
-    }
+        },
+        backToTop: function () {
+            window.scrollTo(0,0);
+        }       
+    },
   });
   
   Vue.config.devtools = true;
